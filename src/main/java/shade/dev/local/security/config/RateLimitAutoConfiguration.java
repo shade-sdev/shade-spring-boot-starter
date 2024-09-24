d872ac8484d62c710e1acde6e1368724fb99ebdc
Shade <Shade@Shade.ga>
2024-09-24 20:56:50 +0400

    JWT feature

--- src/main/java/shade/dev/local/security/config/RateLimitAutoConfiguration.java
+++ src/main/java/shade/dev/local/security/config/RateLimitAutoConfiguration.java
@@ -1,25 +1,29 @@
 package shade.dev.local.security.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import shade.dev.local.security.type.ratelimiting.impl.DefaultRateLimitingCache;
-import shade.dev.local.security.type.ratelimiting.RateLimitingCacheService;
 import shade.dev.local.security.aspect.RateLimitingAspect;
+import shade.dev.local.security.type.ratelimiting.RateLimitingCacheService;
+import shade.dev.local.security.type.ratelimiting.impl.DefaultRateLimitingCache;
 
 @Configuration
 @ConditionalOnMissingBean(RateLimitingCacheService.class)
 public class RateLimitAutoConfiguration {
 
     @Bean
-    public RateLimitingCacheService rateLimitingCache(CacheManager cacheManager) {
+    @ConditionalOnBean(CacheManager.class)
+    public RateLimitingCacheService rateLimitingCache(CacheManager cacheManager)
+    {
         return new DefaultRateLimitingCache(cacheManager);
     }
 
     @Bean
-    public RateLimitingAspect rateLimitingAspect(RateLimitingCacheService cache) {
+    @ConditionalOnBean(RateLimitingCacheService.class)
+    public RateLimitingAspect rateLimitingAspect(RateLimitingCacheService cache)
+    {
         return new RateLimitingAspect(cache);
     }
 
