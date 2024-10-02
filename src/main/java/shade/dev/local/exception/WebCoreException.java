f9fb279783e1ba889d0f691eeda94ffc7a9ac7d8
SAUL <lavish.saulick@elca.mu>
2024-10-02 09:16:47 +0400

    refactor

--- src/main/java/shade/dev/local/exception/WebCoreException.java
+++ src/main/java/shade/dev/local/exception/WebCoreException.java
@@ -1,51 +1,53 @@
 package shade.dev.local.exception;
 
-import shade.dev.local.exception.model.WebErrorCode;
-
 import java.util.UUID;
+
+import org.springframework.http.HttpStatusCode;
+
+import shade.dev.local.exception.model.WebErrorCode;
 
 public class WebCoreException extends RuntimeException {
 
     private final String code;
 
+    private final HttpStatusCode statusCode;
+
     private final WebErrorCode errorCode;
 
-    public WebCoreException(String message, WebErrorCode errorCode)
-    {
+    public WebCoreException(String message, WebErrorCode errorCode) {
         super(message);
         this.errorCode = errorCode;
         this.code = errorCode.getCode();
+        this.statusCode = errorCode.getStatus();
     }
 
-    public WebCoreException(String message, WebErrorCode errorCode, Class<?> clazz)
-    {
+    public WebCoreException(String message, WebErrorCode errorCode, Class<?> clazz) {
         super(String.format("%s %s", clazz.getSimpleName(), message));
         this.errorCode = errorCode;
         this.code = String.format("%s_%s", clazz.getSimpleName().toUpperCase(), errorCode.getCode());
+        this.statusCode = errorCode.getStatus();
     }
 
-    public WebCoreException(String message, WebErrorCode errorCode, UUID id, Class<?> clazz)
-    {
+    public WebCoreException(String message, WebErrorCode errorCode, UUID id, Class<?> clazz) {
         super(String.format("%s (id = %s) %s", clazz.getSimpleName(), id, message));
         this.errorCode = errorCode;
         this.code = String.format("%s_%s", clazz.getSimpleName().toUpperCase(), errorCode.getCode());
+        this.statusCode = errorCode.getStatus();
     }
 
-    public WebCoreException(String message, WebErrorCode errorCode, String idType, UUID id, Class<?> clazz)
-    {
+    public WebCoreException(String message, WebErrorCode errorCode, String idType, UUID id, Class<?> clazz) {
         super(String.format("%s from (%s = %s) %s", clazz.getSimpleName(), idType, id, message));
         this.errorCode = errorCode;
         this.code = String.format("%s_%s", clazz.getSimpleName().toUpperCase(), errorCode.getCode());
+        this.statusCode = errorCode.getStatus();
     }
 
-    public WebErrorCode getErrorCode()
-    {
-        return this.errorCode;
-    }
-
-    public String getCode()
-    {
+    public String getCode() {
         return this.code;
+    }
+
+    public HttpStatusCode getStatusCode() {
+        return this.getStatusCode();
     }
 
 }
