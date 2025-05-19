608986661d4822de0676e6afa73b678cf8119dd6
SAUL <lavish.saulick@elca.mu>
2025-05-19 14:01:51 +0400

    safe casting

--- src/main/java/shade/dev/local/security/type/ratelimiting/impl/DefaultRateLimitingCache.java
+++ src/main/java/shade/dev/local/security/type/ratelimiting/impl/DefaultRateLimitingCache.java
@@ -2,6 +2,7 @@ package shade.dev.local.security.type.ratelimiting.impl;
 
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+
 import shade.dev.local.security.type.ratelimiting.RateLimitingCacheService;
 import shade.dev.local.security.type.ratelimiting.model.RateLimitCounter;
 
@@ -13,23 +14,21 @@ public class DefaultRateLimitingCache implements RateLimitingCacheService {
 
     private final CacheManager cacheManager;
 
-    public DefaultRateLimitingCache(CacheManager cacheManager)
-    {
+    public DefaultRateLimitingCache(CacheManager cacheManager) {
         this.cacheManager = cacheManager;
     }
 
     @Override
-    public void putCounter(String key, RateLimitCounter counter)
-    {
+    public void putCounter(String key, RateLimitCounter counter) {
         Optional.ofNullable(cacheManager.getCache(RATE_CACHE)).ifPresent(c -> c.put(key, counter));
     }
 
     @Override
-    public RateLimitCounter getCounter(String key)
-    {
+    public RateLimitCounter getCounter(String key) {
         return Optional.ofNullable(cacheManager.getCache(RATE_CACHE))
                        .map(cache -> cache.get(key))
                        .map(Cache.ValueWrapper::get)
+                       .filter(RateLimitCounter.class::isInstance)
                        .map(RateLimitCounter.class::cast)
                        .orElse(null);
     }
